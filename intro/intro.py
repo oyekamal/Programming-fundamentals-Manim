33d6aea024172acd6e79e07ea3a6be3cc124f50b
kamalpay <muhammadkamal@paymob.com>
2022-11-05 11:47:21 +0500

    intro done

--- intro/intro.py
+++ intro/intro.py
@@ -1,46 +1,63 @@
 from manim import *
 
+
 class Intro(Scene):
     def construct(self):
-        ok = Text("OK",  t2c={'O':YELLOW}).scale(2)
-        oykamal = Text("oykamal", t2c={'oy':YELLOW}).scale(2)
+        ok = Text("OK",  t2c={'O': YELLOW}).scale(2)
+        oykamal = Text("oykamal", t2c={'oy': YELLOW}).scale(2)
         lets = Text("lets start")
-        
-        
-        message = TextMobject(r"I'm going to teach you the basic concept of ", r"Programming.")
+
+        message = Tex(
+            r"I'm going to teach you the basic concept of ", r"Programming.")
         message[1].set_color(YELLOW)
-        message1 = TextMobject(r"Which are use in lot of ", r"Programming ", r"language.").scale(0.5)
+        message1 = Tex(r"Which are use in lot of ",
+                       r"Programming ", r"language.").scale(0.5)
         message1[1].set_color(YELLOW)
         example = Text("For Example.", color=RED)
+
+        theory = Text("Theory", color=GREEN)
+
+        cross = Cross(mobject=theory, stroke_color=RED, stroke_width=6)
         
-        list_of_content = TextMobject(
-        '$\\bullet$ Display Text on screen \\\\',
-        '$\\bullet$ Variable \\\\',
-        '$\\bullet$ Loop/Nested Loop \\\\',
-        '$\\bullet$ Array \\\\',
-        '$\\bullet$ Condition \\\\',
-        '$\\bullet$ Function \\\\',
-        '$\\bullet$ Class \\\\',
+        see_you = Text("See you in next video", color=GOLD_B)
+        
+        list_of_content = Tex(
+            # '$\\bullet$ Display Text on screen \\\\',
+            '$\\bullet$ Variable \\\\',
+            '$\\bullet$ Condition \\\\',
+            '$\\bullet$ Loop \\\\',
+            '$\\bullet$ Function \\\\',
+            '$\\bullet$ List \\\\',
+            '$\\bullet$ Dictionary \\\\',
         )
-        
-        message.next_to(message1,UP)        
+
+        message.next_to(message1, UP)
         ok.next_to(lets, UP)
-        
+
         self.wait(2)
         # self.play(Write(ok))
-        self.play(Write(oykamal),run_time=5)
+        self.play(Write(oykamal), run_time=3)
         self.wait(2)
-        self.play(ReplacementTransform(oykamal,ok))
+        self.play(ReplacementTransform(oykamal, ok))
         self.play(Write(lets))
-        self.play(FadeOut(ok),FadeOut(lets))
+        self.play(FadeOut(ok), FadeOut(lets))
         self.wait(2)
-        self.play(Write(message),Write(message1))
+
+        self.play(Write(theory), run_time=2)
+        self.wait()
+
+        self.play(Create(cross), run_time=2)
+
+        self.play(FadeOut(theory), FadeOut(cross))
+
+        self.play(Write(message), Write(message1))
         self.wait(3)
-        self.play(FadeOut(message),FadeOut(message1))
+        self.play(FadeOut(message), FadeOut(message1))
         self.play(Write(example))
-        self.play(example.animate.scale(0.4).to_edge(UP).to_edge(LEFT))
+        self.play(example.animate.scale(0.4).to_edge(UL))
         self.wait(1)
         self.play(Write(list_of_content))
         self.wait(2)
-        self.play(list_of_content[0].set_color(RED).animate.to_edge(UP),FadeOut(list_of_content[1:]))
+        # self.play(FadeOut(list_of_content), FadeIn(see_you))
+        self.play(ReplacementTransform(list_of_content, see_you))
         self.wait(2)
